853d652beda741800895704aa038815981b8ef6f
ansarishadabhusain <90554681+ansarishadabhusain@users.noreply.github.com>
2021-09-20 15:00:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ window.addEventListener('scroll', function(){
     head.classList.toggle('sticky', window.scrollY > 0)
 })
 
-// window.addEventListener('click', function(){
-//    const menu = document.querySelector('.menuToggle')
-//    const nav = document.querySelector('.navigation')
-//    menu.classList.toggle('active')
-//    nav.classList.toggle('active')
-// })
-
 function toggleMenu(){
     const menu = document.querySelector('.menuToggle')
     const nav = document.querySelector('.navigation')
